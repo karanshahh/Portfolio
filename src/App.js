585a3447441ab3b52ed08535b706fdfeb28bb22e
karanshahh <karanshah.cs@gmail.com>
2023-01-03 16:34:59 -0500

    navbar almost complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-
+import NavBar from './components/NavBar';
 export default function App() {
   return (
     <div className="App">
-      <h1>portfolio</h1>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+        <span className="logo-name">
+          <i class="fas fa-briefcase"></i>
+          Portfolio
+        </span>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i class="fa-solid fa-bars" style={{ color: "white"}}></i>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Research
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
